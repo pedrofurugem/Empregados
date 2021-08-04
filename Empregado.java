28d5db28c464ccf4d14e3047fb99bbc4e02ebc40
pedrofurugem <61330444+pedrofurugem@users.noreply.github.com>
2021-08-04 15:25:33 -0300

    Calculando salario de empregados

---
+++ Empregado.java
@@ -0,0 +1,63 @@
+public class Empregado {
+      private String nome;
+      private int idade;
+      private double salario;
+      private double comissao;
+      private double bonus;
+      private int tipo;
+
+    public int getTipo(){
+        return this.tipo;
+    }
+
+
+
+    public void setSalario (double salario){
+        this.salario = salario;
+    }
+
+    public void setComissao (double comissao){
+        this.comissao = comissao;
+    }
+
+    public void setBonus (double bonus){
+        this.bonus = bonus;
+    }
+
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+
+
+
+
+    public double calculaSalario(){
+        if (tipo == 1){
+            return salario;
+        }
+        else if (tipo == 2){
+            return salario + salario * comissao;
+        } 
+        else if (tipo == 3){
+            return salario + bonus;
+        }
+        return 0;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+
+}
